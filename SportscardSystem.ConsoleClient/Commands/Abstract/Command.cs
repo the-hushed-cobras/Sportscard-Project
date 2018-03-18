41e44b1a9ca6d93e9a483549d689bdd8a973b80c
Alek Hristov <alek.hristov@yahoo.com>
2018-03-18 02:05:41 +0200

    Refactoring

---
+++ SportscardSystem.ConsoleClient/Commands/Abstract/Command.cs
@@ -0,0 +1,14 @@
+﻿using SportscardSystem.ConsoleClient.Commands.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace SportscardSystem.ConsoleClient.Commands.Abstract
+{
+    public abstract class Command : ICommand
+    {
+        public string Execute(IList<string> parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
