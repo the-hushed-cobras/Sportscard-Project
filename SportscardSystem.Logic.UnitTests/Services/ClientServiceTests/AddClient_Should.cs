cfe47d844e62f987ddc7ed5c72de697d4a1d0cc0
Alek Hristov <alek.hristov@yahoo.com>
2018-03-26 21:36:07 +0300

    Unit tests added for ClientService

---
+++ SportscardSystem.Logic.UnitTests/Services/ClientServiceTests/AddClient_Should.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportscardSystem.Data.Contracts;
+using SportscardSystem.DTO;
+using SportscardSystem.Logic.Services;
+using SportscardSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace SportscardSystem.Logic.UnitTests.Services.ClientServiceTests
+{
+    [TestClass]
+    public class AddClient_Should
+    {
+        [TestMethod]
+        public void AddClientToDabatase_WhenInvokedWithValidParameters()
+        {
+            //Arrange
+            var dbContextMock = new Mock<ISportscardSystemDbContext>();
+            var mapperMock = new Mock<IMapper>();
+            var expectedClient = new Client() { FirstName = "Stamat", LastName = "Stamatov", Age = 25 };
+
+            var data = new List<Client>
+            {
+                new Client { FirstName = "Stamat", LastName = "Stamateev", Age = 26 },
+                new Client { FirstName = "Stamat", LastName = "Stamatev", Age = 27 },
+                new Client { FirstName = "Stamat", LastName = "Stamatav", Age = 28 }
+            };
+        
+            var mockSet = new Mock<DbSet<Client>>();
+
+            mockSet.SetupData(data);
+            mockSet.Setup(m => m.Add(It.IsAny<Client>()));
+
+            dbContextMock
+                .Setup(x => x.Clients)
+                .Returns(mockSet.Object);
+
+            var clientDto = new ClientDto() { FirstName = "Stamat", LastName = "Stamatov", Age = 25 };
+
+            mapperMock
+                .Setup(x => x.Map<Client>(clientDto))
+                .Returns(expectedClient);
+
+            var clientService = new ClientService(dbContextMock.Object, mapperMock.Object);
+
+            //Act
+            clientService.AddClient(clientDto);
+
+            //Assert
+            mockSet.Verify(x => x.Add(expectedClient), Times.Once);
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenInvokedWithInvalidParameter()
+        {
+            //Arrange
+            var dbContextMock = new Mock<ISportscardSystemDbContext>();
+            var mapperMock = new Mock<IMapper>();
+
+            ClientDto clientDto = null;
+            var clientService = new ClientService(dbContextMock.Object, mapperMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(() => clientService.AddClient(clientDto));
+        }
+    }
+}
---
+++ SportscardSystem.Logic.UnitTests/Services/ClientServiceTests/GetAllClients_Should.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportscardSystem.Architecture.Automapper;
+using SportscardSystem.Data.Contracts;
+using SportscardSystem.DTO.Contracts;
+using SportscardSystem.Logic.Services;
+using SportscardSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace SportscardSystem.Logic.UnitTests.Services.ClientServiceTests
+{
+    [TestClass]
+    public class GetAllClients_Should
+    {
+        [TestMethod]
+        public void ReturnIEnumerableOfClientsDto_WhenCollectionIsNotNull()
+        {
+            //Arrange
+            var dbContextMock = new Mock<ISportscardSystemDbContext>();
+            var mapperMock = new Mock<IMapper>();
+
+            var data = new List<Client>
+            {
+                new Client { FirstName = "Stamat", LastName = "Stamateev", Age = 26 },
+                new Client { FirstName = "Stamat", LastName = "Stamatev", Age = 27 },
+                new Client { FirstName = "Stamat", LastName = "Stamatav", Age = 28 }
+            };
+
+            var mockSet = new Mock<DbSet<Client>>();
+
+            mockSet.SetupData(data);
+            Mapper.Reset();
+            AutomapperConfiguration.Initialize();
+
+            dbContextMock
+                .Setup(x => x.Clients)
+                .Returns(mockSet.Object);
+
+            var clientService = new ClientService(dbContextMock.Object, mapperMock.Object);
+
+            //Act
+            var clients = clientService.GetAllClients();
+
+            //Assert
+            Assert.AreEqual(data.Count, clients.Count());
+            Assert.IsNotNull(clients);
+            Assert.IsInstanceOfType(clients, typeof(IEnumerable<IClientDto>));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenClientsIsNull()
+        {
+            //Arrange
+            var dbContextMock = new Mock<ISportscardSystemDbContext>();
+            var mapperMock = new Mock<IMapper>();
+
+            var data = new List<Client>
+            {
+                new Client { FirstName = "Stamat", LastName = "Stamateev", Age = 26 },
+                new Client { FirstName = "Stamat", LastName = "Stamatev", Age = 27 },
+                new Client { FirstName = "Stamat", LastName = "Stamatav", Age = 28 }
+            };
+
+            var mockSet = new Mock<DbSet<Client>>();
+
+            mockSet.SetupData(data);
+            Mapper.Reset();
+            AutomapperConfiguration.Initialize();
+
+            dbContextMock
+                .Setup(x => x.Clients)
+                .Returns((IDbSet<Client>)null);
+
+            var clientService = new ClientService(dbContextMock.Object, mapperMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(() => clientService.GetAllClients());
+        }
+    }
+}
