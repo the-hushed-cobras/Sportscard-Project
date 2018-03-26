e3fcbabbc3667c82fcb287db3f964cbaba776eca
Alek Hristov <alek.hristov@yahoo.com>
2018-03-27 01:05:49 +0300

    Constructor, Add and GetAll methods all tested in all services

---
+++ SportscardSystem.Logic.UnitTests/Services/SportServiceTests/GetAllSports_Should.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportscardSystem.Architecture.Automapper;
+using SportscardSystem.Data.Contracts;
+using SportscardSystem.DTO.Contracts;
+using SportscardSystem.Logic.Services;
+using SportscardSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace SportscardSystem.Logic.UnitTests.Services.SportServiceTests
+{
+    [TestClass]
+    public class GetAllSports_Should
+    {
+        public void ReturnIEnumerableOfSportDto_WhenCollectionIsNotNull()
+        {
+            //Arrange
+            var dbContextMock = new Mock<ISportscardSystemDbContext>();
+            var mapperMock = new Mock<IMapper>();
+
+            var data = new List<Sport>
+            {
+                new Sport { Name = "Gym" },
+                new Sport { Name = "Yoga" },
+                new Sport { Name = "Pilates" }
+            };
+
+            var mockSet = new Mock<DbSet<Sport>>();
+
+            mockSet.SetupData(data);
+            Mapper.Reset();
+            AutomapperConfiguration.Initialize();
+
+            dbContextMock
+                .Setup(x => x.Sports)
+                .Returns(mockSet.Object);
+
+            var sportService = new SportService(dbContextMock.Object, mapperMock.Object);
+
+            //Act
+            var sports = sportService.GetAllSports();
+
+            //Assert
+            Assert.AreEqual(data.Count, sports.Count());
+            Assert.IsNotNull(sports);
+            Assert.IsInstanceOfType(sports, typeof(IEnumerable<ISportDto>));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenSportsIsNull()
+        {
+            //Arrange
+            var dbContextMock = new Mock<ISportscardSystemDbContext>();
+            var mapperMock = new Mock<IMapper>();
+
+            var data = new List<Sport>
+            {
+                new Sport { Name = "Gym" },
+                new Sport { Name = "Yoga" },
+                new Sport { Name = "Pilates" }
+            };
+
+            var mockSet = new Mock<DbSet<Sport>>();
+
+            mockSet.SetupData(data);
+            Mapper.Reset();
+            AutomapperConfiguration.Initialize();
+
+            dbContextMock
+                .Setup(x => x.Sports)
+                .Returns((IDbSet<Sport>)null);
+
+            var sportService = new SportService(dbContextMock.Object, mapperMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(() => sportService.GetAllSports());
+        }
+    }
+}
---
+++ SportscardSystem.Logic.UnitTests/Services/SportshallServiceTests/GetAllSportshalls_Should.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportscardSystem.Architecture.Automapper;
+using SportscardSystem.Data.Contracts;
+using SportscardSystem.DTO.Contracts;
+using SportscardSystem.Logic.Services;
+using SportscardSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace SportscardSystem.Logic.UnitTests.Services.SportshallServiceTests
+{
+    [TestClass]
+    public class GetAllSportshalls_Should
+    {
+        public void ReturnIEnumerableOfSportshallsDto_WhenCollectionIsNotNull()
+        {
+            //Arrange
+            var dbContextMock = new Mock<ISportscardSystemDbContext>();
+            var mapperMock = new Mock<IMapper>();
+
+            var data = new List<Sportshall>
+            {
+                new Sportshall { Name = "Sportshall1" },
+                new Sportshall { Name = "Sportshall2" },
+                new Sportshall { Name = "Sportshall3" }
+            };
+
+            var mockSet = new Mock<DbSet<Sportshall>>();
+
+            mockSet.SetupData(data);
+            Mapper.Reset();
+            AutomapperConfiguration.Initialize();
+
+            dbContextMock
+                .Setup(x => x.Sportshalls)
+                .Returns(mockSet.Object);
+
+            var sportshallService = new SportshallService(dbContextMock.Object, mapperMock.Object);
+
+            //Act
+            var sportshalls = sportshallService.GetAllSportshalls();
+
+            //Assert
+            Assert.AreEqual(data.Count, sportshalls.Count());
+            Assert.IsNotNull(sportshalls);
+            Assert.IsInstanceOfType(sportshalls, typeof(IEnumerable<ISportshallDto>));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenSportshallsIsNull()
+        {
+            //Arrange
+            var dbContextMock = new Mock<ISportscardSystemDbContext>();
+            var mapperMock = new Mock<IMapper>();
+
+            var data = new List<Sportshall>
+            {
+                new Sportshall { Name = "Sportshall1" },
+                new Sportshall { Name = "Sportshall2" },
+                new Sportshall { Name = "Sportshall3" }
+            };
+
+            var mockSet = new Mock<DbSet<Sportshall>>();
+
+            mockSet.SetupData(data);
+            Mapper.Reset();
+            AutomapperConfiguration.Initialize();
+
+            dbContextMock
+                .Setup(x => x.Sportshalls)
+                .Returns((IDbSet<Sportshall>)null);
+
+            var sportService = new SportshallService(dbContextMock.Object, mapperMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(() => sportService.GetAllSportshalls());
+        }
+    }
+}
