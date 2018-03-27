fd4d86ee1d3ced1c0389eee2f0e11eb2c7133ba3
Unknown <wod.moshkin@gmail.com>
2018-03-28 00:08:39 +0300

    GetVisitsBySportHall were added

---
+++ SportscardSystem.ConsoleClient/Commands/Visits/GetVisitsBySporthall.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bytes2you.Validation;
+using SportscardSystem.ConsoleClient.Commands.Abstract;
+using SportscardSystem.ConsoleClient.Commands.Contracts;
+using SportscardSystem.ConsoleClient.Core.Factories.Contracts;
+using SportscardSystem.Logic.Services.Contracts;
+
+namespace SportscardSystem.ConsoleClient.Commands.Visits
+{
+    public class GetVisitsBySporthallCommand : Command, ICommand
+    {
+        private readonly IVisitService visitService;
+
+        public GetVisitsBySporthall(ISportscardFactory sportscardFactory, IVisitService visitService) : base(sportscardFactory)
+        {
+            Guard.WhenArgument(visitService, "Visit Service can not be null.").IsNull().Throw();
+            this.visitService = visitService;
+        }
+
+        public string Execute(IList<string> parameters)
+        {
+            Guard.WhenArgument(parameters.Count, "Count parameter for visits by sportshall cmd shoul be 1.").IsNotEqual(1).Throw();
+
+            string sportshall = parameters[0];
+            Guard.WhenArgument(sportshall, "Sportshall can not be null or empty").IsNotNullOrEmpty().Throw();
+
+            throw new NotImplementedException();
+        }
+    }
+}
