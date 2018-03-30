3d4b79e92347936c9696aed53efb37993aff74a0
Unknown <wod.moshkin@gmail.com>
2018-03-30 15:31:04 +0300

    DeleteClientTestsClass were added

--- SportscardSystem.ConsoleClient/Commands/Delete/DeleteSportshallCommand.cs
+++ SportscardSystem.ConsoleClient/Commands/Delete/DeleteSportshallCommand.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Bytes2you.Validation;
 using SportscardSystem.ConsoleClient.Commands.Abstract;
 using SportscardSystem.ConsoleClient.Commands.Contracts;
