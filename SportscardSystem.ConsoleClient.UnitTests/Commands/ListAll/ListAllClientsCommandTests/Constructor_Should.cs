d0a063e47eefc49b4dc3e3408d1ef4f87c0221e9
Alek Hristov <alek.hristov@yahoo.com>
2018-03-30 20:33:53 +0300

    ListAllClientsCommand unit tests implemented

---
+++ SportscardSystem.ConsoleClient.UnitTests/Commands/ListAll/ListAllClientsCommandTests/Constructor_Should.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportscardSystem.ConsoleClient.Commands.Contracts;
+using SportscardSystem.ConsoleClient.Commands.ListAll;
+using SportscardSystem.Logic.Services.Contracts;
+using System;
+
+namespace SportscardSystem.ConsoleClient.UnitTests.Commands.ListAll.ListAllClientsCommandTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void CreateInstance_WhenInvokedWithValidParameter()
+        {
+            //Arrange
+            var clientServiceMock = new Mock<IClientService>();
+
+            //Act
+            var listAllClientsCommand = new ListAllClientsCommand(clientServiceMock.Object);
+
+            //Assert
+            Assert.IsNotNull(listAllClientsCommand);
+            Assert.IsInstanceOfType(listAllClientsCommand, typeof(ICommand));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenInvokedWithInvalidNullClientSeriveParameter()
+        {
+            //Arrange
+            //var clientServiceMock = new Mock<IClientService>();
+
+            //Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new ListAllClientsCommand(null));
+        }
+    }
+}
