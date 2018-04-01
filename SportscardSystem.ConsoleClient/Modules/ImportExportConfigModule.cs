ef00df134c6c377ba72cfde4ca50d207ec99a7cb
AleksPeneva <alexemilpeneva@abv.bg>
2018-04-01 16:30:03 +0300

    Fixing path to json file

--- SportscardSystem.ConsoleClient/Modules/ImportExportConfigModule.cs
+++ SportscardSystem.ConsoleClient/Modules/ImportExportConfigModule.cs
@@ -14,7 +14,7 @@ namespace SportscardSystem.ConsoleClient.Modules
     public class ImportExportConfigModule : Module
     {
         private const string FileDirectory = "./../../../ ";
-        private const string FilePath = "./../../../Sportscards-Two.json";
+        private const string FilePath = "./../../../Sportscards.json";
 
         protected override void Load(ContainerBuilder builder)
         {
